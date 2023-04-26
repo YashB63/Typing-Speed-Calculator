1f6ffa3ed18c3c81d3db9a87f12872d9ed5b9efa
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-04-26 22:27:59 +0530

    Add files via upload

---
+++ Typing Speed Calculator/typing_speed_calculator.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+import time
+import threading
+import random
+
+
+class TypingSpeedGUI:
+
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.title("Typing SPeed Calculator")
+        self.root.geometry("800x600")
+
+        self.texts = open("sample_text.txt", "r").read().split("\n")
+
+        self.frame = tk.Frame(self.root)
+
+        self.sample_label = tk.Label(self.frame, text=random.choice(self.texts), font=("Helvetica", 18))
+        self.sample_label.grid(row=0, column=0, columnspan=2, padx=5, pady=10)
+
+        self.input_entry = tk.Entry(self.frame, width=40, font=("Helvetica", 24))
+        self.input_entry.grid(row=1, column=0, columnspan=2, padx=5, pady=10)
+        self.input_entry.bind("<KeyRelease>", self.start)
+
+        self.speed_label = tk.Label(self.frame, text="Speed: \n0.00 CPS\n0.00 CPM ", font=("Helvetica", 18))
+        self.speed_label.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
+
+        self.reset_button = tk.Button(self.frame, text="Reset", command=self.reset)
+        self.reset_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
+
+        self.frame.pack(exapnd=True)
+
+        self.counter = 0
+        self.running = False
+
+        self.root.mainloop()
+
+    def start(self, event):
+        if not self.running:
+            if not event. keycode in [16, 17, 18]:
+                self.running = True
+                t = threading.Thread(target=self.time_thread)
+                t.start()
+        if not self.sample_label.cget('text').startswith(self.input_entry.get()):
+            self.input_entry.config(fg="red")
+        else:
+            self.input_entry.config(fg="black")
+        if self.input_entry.get() == self.sample_label.cget('text')[:-1]:
+            self.running = False
+            self.input_entry.config(fg="green")
+
+
+    def time_thread(self):
+        while self.running:
+            time.sleep(0.1)
+            self.counter += 0.1
+            cps = len(self.input_entry.get()) / self.counter
+            cpm = cps * 60
+            wps = len(self.input_entry.get().split(" ")) / self.counter
+            wpm = wps * 60
+            self.speed_label.config(text="Speed: \n{cps:.2f} CPS\n{cpm:.2f} CPM\n{wps:.2f} WPS\n {wpm:.2f} WPM".format(cps, cpm))
+
+
+    def reset(self):
+        self.running = False
+        self.counter = 0
+        self.speed_label.config(text="Speed: \n0.00 CPS\n0.00 CPM\n0.00 WPS\n0.00 WPS")
+        self.sample_label.config(text=random.choice(self.texts))
+        self.input_entry.delete(0, tk.END)
+
+TypingSpeedGUI()
